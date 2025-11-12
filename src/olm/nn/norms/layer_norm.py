55c68f56f12aae0bb9f1bb583bb3f44ce9c5443d
Tavish Mankash <tavish.mankash@gmail.com>
2025-11-12 14:18:34 +0530

    Implemented all norms and HF tokenization
    2 Norms: LayerNorm and RMSNorm
    2 classes for tokenizers: to use a huggingface fast tokenizer, to train a custom tokenizer

---
+++ src/olm/nn/norms/layer_norm.py
@@ -0,0 +1,24 @@
+from torch import nn
+import torch 
+from olm.core.registry import NORMS
+from olm.nn.norms.base import NormBase
+
+@NORMS.register("layer_norm")
+class LayerNorm(NormBase):
+    """LayerNorm layer as described in https://arxiv.org/abs/1607.06450"""
+    def __init__(self, d_model: int, context_length: int, eps: float=1e-5, device=None, dtype=None):
+        super().__init__(d_model, device=device, dtype=dtype)
+        self.eps = eps
+        self.context_length = context_length
+        self.gamma = nn.Parameter(torch.full((d_model,), 1, device=device, dtype=dtype))
+        self.beta = nn.Parameter(torch.full((d_model,), 0, device=device, dtype=dtype))
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        # x: (batch_size, sequence_length, d_model)
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        mean = x.mean(dim=2, keepdim=True)  # (batch_size, sequence_length, 1)
+        variance = x.var(dim=2, keepdim=True, unbiased=False)  # (batch_size, sequence_length, 1)
+        x_normalized = (x - mean) / torch.sqrt(variance + self.eps)  # (batch_size, sequence_length, d_model)
+        result = x_normalized * self.gamma + self.beta  # (batch_size, sequence_length, d_model)
+        return result.to(in_dtype)
