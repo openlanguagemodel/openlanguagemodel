f2b55a39363c91b9443bf453bb9ecda90e07de08
cyai <83634399+cyai@users.noreply.github.com>
2025-11-16 20:20:50 +0530

    feat: add aboslute, alibi and sinusoidal embeddings

--- src/olm/nn/embeddings/positional/__init__.py
+++ src/olm/nn/embeddings/positional/__init__.py
@@ -1,10 +1,13 @@
 # src/olm/nn/embeddings/positional/__init__.py
 from .rope import RotaryPositionalEmbedding, PartialRotaryPositionalEmbedding
-from .absolute import *
-from .alibi import *
-from .sinusoidal import *
+from .absolute import AbsolutePositionalEmbedding
+from .alibi import ALiBiPositionalBias
+from .sinusoidal import SinusoidalPositionalEmbedding
 
 __all__ = [
     "RotaryPositionalEmbedding",
     "PartialRotaryPositionalEmbedding",
+    "AbsolutePositionalEmbedding",
+    "ALiBiPositionalBias",
+    "SinusoidalPositionalEmbedding",
 ]
