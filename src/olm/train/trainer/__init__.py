26f450abdf6aa9e00b25d853a56d873ef474097d
cyai <83634399+cyai@users.noreply.github.com>
2025-12-31 11:13:33 +0530

    refactor: improvise train and add callbacks

--- src/olm/train/trainer/__init__.py
+++ src/olm/train/trainer/__init__.py
@@ -1,1 +1,20 @@
-from .trainer import Trainer
+from olm.train.trainer.trainer import Trainer, TrainerCallback
+from olm.train.callbacks import (
+    ValidationCallback,
+    CheckpointCallback,
+    LRMonitorCallback,
+    MetricsLoggerCallback,
+    EarlyStoppingCallback,
+    ThroughputCallback,
+)
+
+__all__ = [
+    "Trainer",
+    "TrainerCallback",
+    "ValidationCallback",
+    "CheckpointCallback",
+    "LRMonitorCallback",
+    "MetricsLoggerCallback",
+    "EarlyStoppingCallback",
+    "ThroughputCallback",
+]
