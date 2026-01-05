962e423f6f4b60d012e92bdd8e4387a3646b4b68
Keshava Prasad <keshava.pprsd15@gmail.com>
2026-01-06 00:15:43 +0530

    fix: cleaned up README.md and updated docstring

--- src/olm/nn/torch_nn_wrappers.py
+++ src/olm/nn/torch_nn_wrappers.py
@@ -6,3 +6,12 @@ class Linear(nn.Linear):
     
     def forward(self, x):
         return super().forward(x)
+    
+Block([
+    Block([
+        Embedding(vocab_size, embed_dim),
+        AbsolutePositionalEmbedding(max_seq_len, embed_dim, dropout)
+    ]),
+    Repeat(lambda: GPT2Block(embed_dim, num_heads, dropout), num_layers),
+    OutputHead(embed_dim, vocab_size)
+])
