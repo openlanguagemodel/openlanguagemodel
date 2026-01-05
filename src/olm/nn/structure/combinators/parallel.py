7dabb1d0e850db1dbdb2c957cbf1fe39b1f18e4e
Keshava Prasad <keshava@Keshavas-MacBook-Pro.local>
2026-01-06 00:15:43 +0530

    fix: cleaned up README.md and updated docstring

--- src/olm/nn/structure/combinators/parallel.py
+++ src/olm/nn/structure/combinators/parallel.py
@@ -4,7 +4,30 @@ import torch.nn as nn
 from typing import List, Callable, Union
 
 class Parallel(BaseCombinator):
+    """
+    Apply multiple blocks to the same input and merge their outputs.
+
+    The merge function takes a list of tensors and a dimension argument.
+
+    Args:
+        blocks: Modules applied in parallel to the same input.
+        merge: Function that combines the list of outputs and a dimension.
+        dim: Dimension used by the merge function when applicable.
+
+    Attributes:
+        blocks: ModuleList storing the parallel blocks.
+        merge: Merge function used to combine outputs.
+        dim: Dimension passed to the merge function.
+    """
     def __init__(self, blocks: List[nn.Module], merge: Callable = None, dim: int = -1):
+        """
+        Initialize the parallel combinator.
+
+        Args:
+            blocks: Modules applied in parallel to the same input.
+            merge: Function that combines the list of outputs and a dimension.
+            dim: Dimension used by the merge function when applicable.
+        """
         super().__init__()
 
         self.blocks = nn.ModuleList(blocks)
@@ -12,10 +35,17 @@ class Parallel(BaseCombinator):
         self.dim = dim
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Apply all blocks in parallel and merge their outputs.
+
+        Args:
+            x: Input tensor.
+
+        Returns:
+            Merged output tensor.
+        """
         outputs = []
         for block in self.blocks:
             outputs.append(block(x))
 
         return self.merge(outputs, self.dim)
-
-
