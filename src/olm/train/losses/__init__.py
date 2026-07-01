379cde8c1387cb02d1602ab9430c1ca30fd16dbc
cyai <83634399+cyai@users.noreply.github.com>
2026-07-01 14:41:16 +0530

    chore: update package exports for new components

--- src/olm/train/losses/__init__.py
+++ src/olm/train/losses/__init__.py
@@ -5,8 +5,8 @@ from olm.train.losses.cross_entropy import CrossEntropyLoss
 from olm.train.losses.kllloss import KLLoss
 from olm.train.losses.mce import MaskedCELoss
 from olm.train.losses.zloss import ZLoss
-from olm.train.losses.load_balance_loss import LoadBalanceLoss
-from olm.train.losses.mtp_loss import MTPLoss
+from olm.train.losses.load_balance import LoadBalanceLoss
+from olm.train.losses.mtp import MTPLoss
 
 __all__ = [
     "LossBase",
