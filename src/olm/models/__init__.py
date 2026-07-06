6add01659ce4b6ac5dac26211b08b121e13aa6c3
cyai <83634399+cyai@users.noreply.github.com>
2026-07-07 03:27:11 +0530

    feat: register all new model architectures

--- src/olm/models/__init__.py
+++ src/olm/models/__init__.py
@@ -7,10 +7,13 @@ from olm.models.alibaba import (
     Qwen2_5_14B,
     Qwen2_5_32B,
     Qwen2_5_72B,
+    Qwen3_5_Model,
+    Qwen3_5_397B,
 )
 from olm.models.allenai import OLMoModel, OLMo_7B
 from olm.models.facebook import OPTModel, OPT125M
 from olm.models.google import Gemma2Model, Gemma2_2B, Gemma2_9B, Gemma2_27B
+from olm.models.inclusionai import Ling2_5_Model, Ling2_5_1T
 from olm.models.meta import (
     Llama2Model,
     Llama2_7B,
@@ -24,7 +27,11 @@ from olm.models.meta import (
     Llama3_2_3B,
 )
 from olm.models.microsoft import Phi3Model, Phi3_5_Mini, Phi3_Small, Phi4Model, Phi4_14B
+from olm.models.minimax import MiniMaxM2_5_Model, MiniMaxM2_5_230B
+from olm.models.nanbeige import Nanbeige4_1_Model, Nanbeige4_1_3B
 from olm.models.openai import GPT2Model, GPT2, GPT2Medium, GPT2Large, GPT2XL
+from olm.models.sarvam import Sarvam30B_Model, Sarvam30B, Sarvam105B_Model, Sarvam105B
+from olm.models.stepfun import Step3_5_Flash_Model, Step3_5_Flash_196B
 
 __all__ = [
     "GPT2Model",
@@ -50,6 +57,8 @@ __all__ = [
     "Qwen2_5_14B",
     "Qwen2_5_32B",
     "Qwen2_5_72B",
+    "Qwen3_5_Model",
+    "Qwen3_5_397B",
     "Phi3Model",
     "Phi3_5_Mini",
     "Phi3_Small",
@@ -63,4 +72,16 @@ __all__ = [
     "OLMo_7B",
     "OPTModel",
     "OPT125M",
+    "Step3_5_Flash_Model",
+    "Step3_5_Flash_196B",
+    "MiniMaxM2_5_Model",
+    "MiniMaxM2_5_230B",
+    "Nanbeige4_1_Model",
+    "Nanbeige4_1_3B",
+    "Ling2_5_Model",
+    "Ling2_5_1T",
+    "Sarvam30B_Model",
+    "Sarvam30B",
+    "Sarvam105B_Model",
+    "Sarvam105B",
 ]
--- src/olm/models/alibaba/__init__.py
+++ src/olm/models/alibaba/__init__.py
@@ -3,6 +3,7 @@ from .qwen2 import (
     Qwen2_5_72B, Qwen2_5_32B, Qwen2_5_14B, Qwen2_5_7B, 
     Qwen2_5_3B, Qwen2_5_1_5B, Qwen2_5_0_5B
 )
+from .qwen3_5 import Qwen3_5_Model, Qwen3_5_397B
 
 __all__ = [
     'Qwen2Model',
@@ -12,5 +13,7 @@ __all__ = [
     'Qwen2_5_7B', 
     'Qwen2_5_3B', 
     'Qwen2_5_1_5B', 
-    'Qwen2_5_0_5B'
+    'Qwen2_5_0_5B',
+    'Qwen3_5_Model',
+    'Qwen3_5_397B',
 ]
